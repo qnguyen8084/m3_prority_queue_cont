6ad852e2ab75c6a48d7d9de97f006edefe134fb7
Quy Nguyen <qnguyen8084@sdsu.edu>
2024-09-09 09:08:57 -0700

    saving before merging rebasing on top of master

--- src/test/java/mypriorityqueue/StudentTest.java
+++ src/test/java/mypriorityqueue/StudentTest.java
@@ -10,6 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
 
+    Student hanna = new Student("hanna", 123, "hanna@sdsu.edu", 3.3F, 120);
+    Student jesse = new Student("jesse", 1234, "hanna@sdsu.edu", 3.3F, 120);
+    Student adam = new Student("adam", 124, "adam@sdsu.edu", 3.4F, 120);
+    Student bob = new Student("bob", 123, "bob@sdsu.edu", 3.3F, 121);
+    Student chris = new Student("chris", 123, "chris@sdsu.edu", 3.2F, 120);
 
     @Test
     void calculatePriority() {
